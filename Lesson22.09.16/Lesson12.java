bbbff78267d7c34af7b367b6e68d09bbda0af4e7
blvvvck <theblvvvck@gmail.com>
2016-09-28 21:29:06 +0300

    Update Lesson12.java

--- Lesson22.09.16/Lesson12.java
+++ Lesson22.09.16/Lesson12.java
@@ -1,14 +1,14 @@
 public class Lesson12{
 	public static void main(String[] args){
 		int n = Integer.parseInt(args[0]);
-		int n1 = n;
+		int n1 = n - 1;
 
 		if (n1 < 0){
 			System.out.println("The number must be positive");
 		}
 
 		for (int i = 1; i <= n; i++){
-			for (int j = n1-1; j >= 1; j--){
+			for (int j = n1; j >= 1; j--){
 				System.out.print(" ");
 			}
 
