9b67d324c77dd489228dba5b1e0726bf1acc35c6
Rinat <theblvvvck@gmail.com>
2016-10-05 23:46:56 +0300

    Update Task1.java

--- Homework29.09.16/Task1.java
+++ Homework29.09.16/Task1.java
@@ -22,7 +22,7 @@ public class Task1 {
 
 			for (int i = 0; i < a.length - 1; i++){
 
-				for (int j = 0; j < a.length-i-1; j++) {
+				for (int j = 0; j < a.length-1; j++) {
 
 					if (a[j] > a[j+1]) {
 
